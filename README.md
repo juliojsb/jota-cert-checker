4528784cbbc756cc0e39023cc23fbfadf48b3bf5
juliojsb <jsb.inform@gmail.com>
2018-02-11 21:46:36 +0100

    Added HTML and terminal modes, option to send email

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# ssl-certs-management
-Repository of scripts to manage SSL certificates
+# jota-cert-checker
+
+## Description
+
+A script to check SSL certificate expiration date of a list of sites.
+
+The script can be launched in two modes:
+
+* **Terminal**: Output is displayed in your terminal
+* **HTML**: the script generates an HTML file (called **certs_check.html** by default) that can be opened with your browser. 
+
+Optionally, you can also embed the HTML and send it to an email (you will need to install **mutt** if you use this option)
+
+## Usage
+
+For example, we have the following file called sitelist that contains a list of domains with the HTTPS port, one domain per line:
+
+```
+linux.com:443
+kernel.org:443
+gnu.org:443
+debian.org:443
+ubuntu.com:443
+github.com:443
+google.es:443
+redhat.com:443
+superuser.com:443
+youtube.com:443
+stackoverflow.com:443
+stackexchange.com:443
+wikipedia.org:443
+python.org:443
+codecademy.com:443
+packtpub.com:443
+reddit.com:443
+mysql.com:443
+```
+
+In the following cases I modified the variables **warning_days** and **alert_days** for sample purposes. 
+
+To launch the script in terminal mode:
+```bash
+./jota-cert-checker.sh -f sitelist -o terminal
+```
+We get the following output in our terminal:
+
+![screenshot from 2018-02-11 20-33-06](https://user-images.githubusercontent.com/12804701/36077449-5f85d338-0f6b-11e8-991d-1ffef916d4b6.png)
+
+In HTML mode:
+```bash
+./jota-cert-checker.sh -f sitelist -o html
+```
+We get the following output:
+
+![screenshot from 2018-02-11 20-29-44](https://user-images.githubusercontent.com/12804701/36077452-6c282e4c-0f6b-11e8-966b-f3d863298586.png)
+```bash
+./jota-cert-checker.sh -f sitelist -o html -m mail@example.com
+```
+Checking our email we will see:
+
+![screenshot from 2018-02-11 20-30-11](https://user-images.githubusercontent.com/12804701/36078161-891bb566-0f73-11e8-984c-1cd65127a8e4.png)
--- jota-cert-checker.sh
+++ jota-cert-checker.sh
@@ -4,17 +4,10 @@
 # Website       :www.elarraydejota.com
 # Email         :juliojosesb@gmail.com
 # Description   :Script to check SSL certificate expiration date of a list of sites. Recommended to use with a dark terminal theme to
-#                see the colors correctly
-# Dependencies  :openssl
-# Usage         :./ssl-cert-expiration-checker.sh [ file ]
-#                Where 'file' is a list of sites with one site per line with format 'site:port', example:
-#                google.com:443
-#                microsoft.com:443
-#                github.com:443
-#                ...
+#                see the colors correctly. The terminal also needs to support 256 colors.
+# Dependencies  :openssl, mutt (if you use the mail option)
 # License       :GPLv3
 #
-
 
 #
 # VARIABLES
@@ -22,75 +15,203 @@
 
 sites_list="$1"
 sitename=""
+html_file="certs_check.html"
 current_date=$(date +%s)
 end_date=""
 days_left=""
 certificate_last_day=""
 warning_days="30"
 alert_days="15"
-ok_color="\e[92m"
-warning_color="\e[93m"
-alert_color="\e[91m"
-expired_color="\e[31m"
+# Terminal colors
+ok_color="\e[38;5;40m"
+warning_color="\e[38;5;220m"
+alert_color="\e[38;5;208m"
+expired_color="\e[38;5;196m"
 end_of_color="\033[0m"
 
 #
 # FUNCTIONS
 #
 
-check_ssl_cert(){
-    printf "\n| %-30s | %-30s | %-10s | %-5s %s\n" "SITE" "EXPIRATION DAY" "DAYS LEFT" "STATUS"
+html_mode(){
+	# Generate and reset file
+	cat <<- EOF > $html_file
+	<!DOCTYPE html>
+	<html>
+			<head>
+			<title>SSL Certs expiration</title>
+			</head>
+			<body style="background-color: lightblue;">
+					<h1 style="color: navy;text-align: center;font-family: 'Helvetica Neue', sans-serif;font-size: 20px;font-weight: bold;">SSL Certs expiration checker</h1>
+					<table style="background-color: #C5E1E7;padding: 10px;box-shadow: 5px 10px 18px #888888;margin-left: auto ;margin-right: auto ;border: 1px solid black;">
+					<tr style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;">
+					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Site</th>
+					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Expiration date</th>
+					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Days left</th>
+					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Status</th>
+					</tr>
+	EOF
 
-    while read site;do
-        sitename=$(echo $site | cut -d ":" -f1)
-        certificate_last_day=$(echo | openssl s_client -servername ${site} -connect ${site} 2>/dev/null | \
-        openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
-        end_date=$(date +%s -d "$certificate_last_day")
-        days_left=$(((end_date - current_date) / 86400))
+	while read site;do
+		sitename=$(echo $site | cut -d ":" -f1)
+		certificate_last_day=$(echo | openssl s_client -servername ${site} -connect ${site} 2>/dev/null | \
+		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
+		end_date=$(date +%s -d "$certificate_last_day")
+		days_left=$(((end_date - current_date) / 86400))
 
-        if [ "$days_left" -gt "$warning_days" ];then
-            printf "${ok_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" "$sitename" "$certificate_last_day" "$days_left" "Ok"
-        elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
-            printf "${warning_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" "$sitename" "$certificate_last_day" "$days_left" "Warning"
-        elif [ "$days_left" -le "$alert_days" ] && [ "$days_left" -gt 0 ];then
-            printf "${alert_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" "$sitename" "$certificate_last_day" "$days_left" "Alert"
-        elif [ "$days_left" -le 0 ];then
-            printf "${expired_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" "$sitename" "$certificate_last_day" "$days_left" "Expired"
-        fi
+		if [ "$days_left" -gt "$warning_days" ];then
+			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${sitename}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${certificate_last_day}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${days_left}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">Ok</td>" >> $html_file
+			echo "</tr>" >> $html_file
 
-    done < $sites_list
+		elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
+			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${sitename}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${certificate_last_day}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${days_left}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FFE032;\">Warning</td>" >> $html_file
+			echo "</tr>" >> $html_file
 
-    printf "\n %-10s" "STATUS LEGEND"
-    printf "\n ${ok_color}%-8s${end_of_color} %-30s" "Ok" "- More than ${warning_days} days left until the certificate expires"
-    printf "\n ${warning_color}%-8s${end_of_color} %-30s" "Warning" "- The certificate will expire in less than ${warning_days} days"
-    printf "\n ${alert_color}%-8s${end_of_color} %-30s" "Alert" "- The certificate will expire in less than ${alert_days} days"
-    printf "\n ${expired_color}%-8s${end_of_color} %-30s\n\n" "Expired" "- The certificate has already expired"
+		elif [ "$days_left" -le "$alert_days" ] && [ "$days_left" -gt 0 ];then
+			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${sitename}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${certificate_last_day}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${days_left}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">Alert</td>" >> $html_file
+			echo "</tr>" >> $html_file
+
+		elif [ "$days_left" -le 0 ];then
+			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${sitename}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${certificate_last_day}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${days_left}</td>" >> $html_file
+			echo "<td style=\"padding: 8px;background-color: #EF3434;\">Expired</td>" >> $html_file
+			echo "</tr>" >> $html_file
+
+		fi
+	done < ${sites_list}
+
+	# Close main HTML tags
+	cat <<- EOF >> $html_file
+			</table>
+			</body>
+	</html>
+	EOF
 }
 
-how_to_use(){
-    echo "Usage: $0 [ file ]"
-    echo "Check SSL certificate expiration date from a list of sites"
-    echo ""
-    echo "In 'file' there must be a list of one site per line with the format sitename:port, for example:"
-    echo "google.com:443"
-    echo "linux.com:443"
-    echo "..."
-    echo ""
-    echo "Example: ./ssl-cert-expiration-checker.sh sitelist"
+terminal_mode(){
+	printf "\n| %-30s | %-30s | %-10s | %-5s %s\n" "SITE" "EXPIRATION DAY" "DAYS LEFT" "STATUS"
+
+	while read site;do
+		sitename=$(echo $site | cut -d ":" -f1)
+		certificate_last_day=$(echo | openssl s_client -servername ${site} -connect ${site} 2>/dev/null | \
+		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
+		end_date=$(date +%s -d "$certificate_last_day")
+		days_left=$(((end_date - current_date) / 86400))
+
+		if [ "$days_left" -gt "$warning_days" ];then
+			printf "${ok_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+			"$sitename" "$certificate_last_day" "$days_left" "Ok"
+
+		elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
+			printf "${warning_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+			"$sitename" "$certificate_last_day" "$days_left" "Warning"
+
+		elif [ "$days_left" -le "$alert_days" ] && [ "$days_left" -gt 0 ];then
+			printf "${alert_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+			"$sitename" "$certificate_last_day" "$days_left" "Alert"
+
+		elif [ "$days_left" -le 0 ];then
+			printf "${expired_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+			"$sitename" "$certificate_last_day" "$days_left" "Expired"
+		fi
+
+	done < $sites_list
+
+	printf "\n %-10s" "STATUS LEGEND"
+	printf "\n ${ok_color}%-8s${end_of_color} %-30s" "Ok" "- More than ${warning_days} days left until the certificate expires"
+	printf "\n ${warning_color}%-8s${end_of_color} %-30s" "Warning" "- The certificate will expire in less than ${warning_days} days"
+	printf "\n ${alert_color}%-8s${end_of_color} %-30s" "Alert" "- The certificate will expire in less than ${alert_days} days"
+	printf "\n ${expired_color}%-8s${end_of_color} %-30s\n\n" "Expired" "- The certificate has already expired"
 }
 
-#
+howtouse(){
+	cat <<-'EOF'
+
+	You must always specify -f option with the name of the file that contains the list of sites to check
+	Options:
+		-f [ sitelist file ]          list of sites (domains) to check
+		-o [ html | terminal ]        output (can be html or terminal)
+		-m [ mail ]                   mail address to send the graphs to
+		-h                            help
+	
+	Examples:
+
+		# Launch the script in terminal mode:
+		./jota-cert-checker.sh -f sitelist -o terminal
+
+		# Using HTML mode:
+		./jota-cert-checker.sh -f sitelist -o html
+
+		# Using HTML mode and sending results via email
+		./jota-cert-checker.sh -f sitelist -o html -m mail@example.com
+
+	EOF
+}
+
+# 
 # MAIN
-#
+# 
 
-if [ $# -ne 1 ];then
-    how_to_use
-else
-    if [ ! -f $sites_list ];then
-        echo "$0: The file '$sites_list' does not exist"
-        how_to_use
-        exit 1
-    else
-        check_ssl_cert
-    fi
-fi 
+if [ "$#" -eq 0 ];then
+	howtouse
+
+elif [ "$#" -ne 0 ];then
+	while getopts ":f:o:m:h" opt; do
+		case $opt in
+			"f")
+				sites_list="$OPTARG"
+				;;
+			"o")
+				output="$OPTARG"
+				if [ "$output" == "terminal" ];then
+					terminal_mode
+				elif [ "$output" == "html" ];then
+					html_mode
+				else
+					echo "Wrong output selected"
+					howtouse
+				fi
+				;;
+			"m")
+				if [ "$output" == "html" ];then
+					mail_to="$OPTARG"
+				else
+					echo "Mail option is only used with HTML mode"
+				fi
+				;;
+			\?)
+				echo "Invalid option: -$OPTARG" >&2
+				howtouse
+				exit 1
+				;;
+			:)
+				echo "Option -$OPTARG requires an argument." >&2
+				howtouse
+				exit 1
+				;;
+			"h" | *)
+				howtouse
+				exit 1
+				;;
+		esac
+	done
+
+	# Send mail if specified
+	if [[ $mail_to ]];then
+		mutt -e 'set content_type="text/html"' $mail_to -s "SSL certs expiration check" < $html_file
+	fi
+fi
