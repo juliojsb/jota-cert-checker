74ed11b70bbf5839ab89ee59eabe66859c72d407
juliojsb <jsb.inform@gmail.com>
2020-06-02 18:19:14 +0200

    Changed openssl parameter -servername to use sitename instead of site

--- jota-cert-checker.sh
+++ jota-cert-checker.sh
@@ -54,7 +54,7 @@ html_mode(){
 
 	while read site;do
 		sitename=$(echo $site | cut -d ":" -f1)
-		certificate_last_day=$(echo | openssl s_client -servername ${site} -connect ${site} 2>/dev/null | \
+		certificate_last_day=$(echo | openssl s_client -servername ${sitename} -connect ${site} 2>/dev/null | \
 		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
 		end_date=$(date +%s -d "$certificate_last_day")
 		days_left=$(((end_date - current_date) / 86400))
@@ -107,7 +107,7 @@ terminal_mode(){
 
 	while read site;do
 		sitename=$(echo $site | cut -d ":" -f1)
-		certificate_last_day=$(echo | openssl s_client -servername ${site} -connect ${site} 2>/dev/null | \
+		certificate_last_day=$(echo | openssl s_client -servername ${sitename} -connect ${site} 2>/dev/null | \
 		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
 		end_date=$(date +%s -d "$certificate_last_day")
 		days_left=$(((end_date - current_date) / 86400))
