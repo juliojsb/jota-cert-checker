b4da31c8f8009e37e538886540d2008e3aafad71
juliojsb <jsb.inform@gmail.com>
2021-04-03 14:28:32 +0200

    Modified formatting in terminal mode

--- jota-cert-checker.sh
+++ jota-cert-checker.sh
@@ -126,7 +126,7 @@ html_mode(){
 }
 
 terminal_mode(){
-	printf "\n| %-30s | %-30s | %-30s | %-10s | %-5s %s\n" "SITE" "ISSUER" "EXPIRATION DAY" "DAYS LEFT" "STATUS"
+	printf "\n| %-25s | %-75s | %-25s | %-10s | %-5s %s\n" "SITE" "ISSUER" "EXPIRATION DAY" "DAYS LEFT" "STATUS"
 
 	while read site;do
 		sitename=$(echo $site | cut -d ":" -f1)
@@ -138,28 +138,28 @@ terminal_mode(){
 			certificate_last_day=$(echo | openssl s_client -servername ${sitename} -connect ${site} 2>/dev/null | \
 			openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
 			issuer=$(echo -e "$cert_out" | grep issuer= | cut -d"=" -f2-)
-			issuer_trim=$(echo -e "$issuer" | cut -c -30)
+			issuer_trim=$(echo -e "$issuer" | awk -F'O =' '{print $2}' | sed 's/^ //g' | cut -c -75)
 			end_date=$(date +%s -d "$certificate_last_day")
 			days_left=$(((end_date - current_date) / 86400))
 		
 			if [ "$days_left" -gt "$warning_days" ];then
-				printf "${ok_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+				printf "${ok_color}| %-25s | %-75s | %-25s | %-10s | %-5s %s\n${end_of_color}" \
 				"$sitename" "$issuer_trim" "$certificate_last_day" "$days_left" "Ok"
 
 			elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
-				printf "${warning_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+				printf "${warning_color}| %-25s | %-75s | %-25s | %-10s | %-5s %s\n${end_of_color}" \
 				"$sitename" "$issuer_trim" "$certificate_last_day" "$days_left" "Warning"
 
 			elif [ "$days_left" -le "$alert_days" ] && [ "$days_left" -gt 0 ];then
-				printf "${alert_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+				printf "${alert_color}| %-25s | %-75s | %-25s | %-10s | %-5s %s\n${end_of_color}" \
 				"$sitename" "$issuer_trim" "$certificate_last_day" "$days_left" "Alert"
 
 			elif [ "$days_left" -le 0 ];then
-				printf "${expired_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+				printf "${expired_color}| %-25s | %-75s | %-25s | %-10s | %-5s %s\n${end_of_color}" \
 				"$sitename" "$issuer_trim" "$certificate_last_day" "$days_left" "Expired"
 			fi
 		else
-			printf "${unknown_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
+			printf "${unknown_color}| %-25s | %-75s | %-25s | %-10s | %-5s %s\n${end_of_color}" \
 			"$sitename" "n/a" "n/a" "n/a" "Unknown"
 		fi
 	done < $sites_list
